ee5a289eedadc10c38cb894e750135106ba88192
marksupharat <mark.supharat@gmail.com>
2016-10-14 12:53:02 +0700

    add test Pattern1

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,9 +1,3 @@
-function Captcha(pattern,operater,leftoperand,rightoperand){
-  this.generate = function(){
-    return new LeftOperand(pattern,leftoperand)+new Operater(operater)+new RightOperand(pattern,rightoperand);
-  }
-}
-
 function Operater(operater){
   this.toString = function(){
     if(operater===1){
@@ -99,12 +93,71 @@ function RightOperand(pattern,ro){
   }
 }
 
+function Captcha(pattern,operater,leftoperand,rightoperand){
+  this.generate = function(){
+    return new LeftOperand(pattern,leftoperand)+new Operater(operater)+new RightOperand(pattern,rightoperand);
+  }
+}
+
 describe('Captcha', function(){
   pat1=1;
   pat2=2;
+  //Pattern1
+  //1
   it('Pattern1',function(){
     var capt = new Captcha(pat1,1,0,0);
     expect(capt.generate()).toEqual('0+Zero');
   });
+  //2
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,1,1,1);
+    expect(capt.generate()).toEqual('1+One');
+  });
+  //3
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,1,2,1);
+    expect(capt.generate()).toEqual('2+One');
+  });
+  //4
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,1,3,4);
+    expect(capt.generate()).toEqual('3+Four');
+  });
+  //5
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,1,5,6);
+    expect(capt.generate()).toEqual('5+Six');
+  });
+  //6
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,2,1,1);
+    expect(capt.generate()).toEqual('1-One');
+  });
+  //7
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,2,3,6);
+    expect(capt.generate()).toEqual('3-Six');
+  });
+  //8
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,2,4,5);
+    expect(capt.generate()).toEqual('4-Five');
+  });
+  //9
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,2,5,7);
+    expect(capt.generate()).toEqual('5-Seven');
+  });
+  //10
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,2,6,0);
+    expect(capt.generate()).toEqual('6-Zero');
+  });
+  //11
+  it('Pattern1',function(){
+    var capt = new Captcha(pat1,2,7,1);
+    expect(capt.generate()).toEqual('7-One');
+  });
+
 
 });
